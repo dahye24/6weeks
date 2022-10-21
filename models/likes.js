2aaae413949ead5d93ba9b47292ecb9216d2b27c
tastekim <tastekim@kakao.com>
2022-10-21 17:38:02 +0900

    DB migration 파일 수정

--- models/likes.js
+++ models/likes.js
@@ -1,24 +1,29 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Likes extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Likes extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  Likes.init({
-    postId: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Likes',
-  });
-  return Likes;
+
+    Likes.init({
+        likeId: {
+            primaryKey: true,
+            type      : DataTypes.INTEGER,
+        },
+        postId: DataTypes.INTEGER,
+        userId: DataTypes.INTEGER
+    }, {
+        sequelize,
+        modelName: 'Likes',
+    });
+    return Likes;
 };
--- models/pets.js
+++ models/pets.js
@@ -1,25 +1,30 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Pets extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Pets extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  Pets.init({
-    petname: DataTypes.STRING,
-    petbirth: DataTypes.STRING,
-    typeofpet: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Pets',
-  });
-  return Pets;
+
+    Pets.init({
+        petId    : {
+            primaryKey: true,
+            type      : DataTypes.INTEGER,
+        },
+        petname  : DataTypes.STRING,
+        petbirth : DataTypes.STRING,
+        typeofpet: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'Pets',
+    });
+    return Pets;
 };
