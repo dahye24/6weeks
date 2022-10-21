139797292b38a0d7934ec8c4310d4e3e85c21cc2
tastekim <tastekim@kakao.com>
2022-10-22 03:30:37 +0900

    login & signup 구현

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,10 @@ const express = require('express');
 const router = express.Router();
 const cookieParser = require('cookie-parser');
 const User = require('./user.route');
-const Post = require('./post.route');
-const Comment = require('./comment.route');
 
 router.use(cookieParser());
 router.use(express.json());
 
-router.use('/', User);
-router.use('/posts', Post);
-router.use('/comments', Comment)
+router.use(User);
 
 module.exports = router;
--- services/user.service.js
+++ services/user.service.js
@@ -1,18 +1,61 @@
 const UserRepository = require('../repositories/user.repository');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+// 암호화 연산 10회 설정
+const salt = 10;
+
+require('dotenv').config();
 
 class UserController {
-  userRepository = new UserRepository();
+    userRepository = new UserRepository();
 
-  createUser = async (loginId, nickname, password) => {
-    const userData = await this.userRepository.createUser(loginId, nickname, password);
+    createUser = async (loginId, nickname, password) => {
+        try {
+            // password 암호화.
+            const hash = await bcrypt.hash(password, salt);
+            // hash 된 비밀번호로 회원정보 저장.
+            const userData = await this.userRepository.createUser(loginId, nickname, hash);
 
-    return {
-      userId: userData.userId,
-      loginId: userData.loginId,
-      nickname: userData.nickname,
-      createdAt: userData.createdAt,
-    }
-  }
+            return {
+                userId   : userData.null,
+                loginId  : userData.loginId,
+                nickname : userData.nickname,
+                createdAt: userData.createdAt,
+            };
+        } catch (err) {
+            err = new Error(`UserService Error`);
+            err.statusCode = 500;
+            throw err;
+        }
+
+    };
+
+    loginUser = async (loginId, password) => {
+        try {
+            // 비밀번호 salt로 hash 하기.
+            const hash = await bcrypt.hash(password, salt);
+            // loginId로 DB에 있는 회원 정보 가져오기.
+            const userData = await this.userRepository.loginUser(loginId);
+            // 가져온 회원 정보에 있는 hash 된 비밀번호와 위에서 hash 한 비밀번호가 일치하는지 확인.
+            const match = await bcrypt.compare(userData.password, hash);
+
+            if (!match) {
+                const err = new Error(`비밀번호가 일치하지 않습니다.`);
+                err.statusCode = 400;
+                throw err;
+            }
+            // 유효시간이 24시간인 token 발급.
+            return {
+                token: jwt.sign(
+                    {userId: userData.userId, loginId: userData.loginId},
+                    process.env.SECRETKEY,
+                    {expiresIn: '24h'})
+            };
+        } catch (err) {
+            throw err;
+        }
+
+    };
 }
 
 module.exports = UserController;
