1cd039213a68d57f4095d2199de3ea6c9b0326d2
yunjin5450 <yunjin5450@gmail.com>
2022-10-23 02:06:16 +0900

    미들웨어수정

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,33 +1,26 @@
 require('dotenv').config();
-const jwt = require("jsonwebtoken");
-const {Users} = require("../models");
+const jwt = require('jsonwebtoken');
+const { Users } = require('../models');
 
 module.exports = (req, res, next) => {
-    try {
-        const cookies = req.cookies['token'];
-        if (!cookies) {
-            return res.status(403).send({
-                errorMessage: '로그인이 필요한 기능입니다.',
-            });
-        }
-        
-        const {token} = cookies
-        console.log(token)
-
-        const {userId} = jwt.verify(token, process.env.SECRETKEY);
-        console.log(userId)
-
-        const user = Users.findByPk(userId).then(user=>{
-            res.locals.user = user;
-            console.log(user)
-            next();
-        })
-        
-
-        
-    } catch (err) {
-        res.status(401).json({
-            errorMessage: "로그인 후 이용이 가능합니다.",
-        });
+  try {
+    const cookies = req.cookies['token'];
+    if (!cookies) {
+      return res.status(403).send({
+        errorMessage: '로그인이 필요한 기능입니다.',
+      });
     }
-}
+
+    const { token } = cookies;
+
+    const { userId } = jwt.verify(token, process.env.SECRETKEY);
+
+    const user = Users.findByPk(userId);
+    res.locals.user = user;
+    next();
+  } catch (err) {
+    res.status(401).json({
+      errorMessage: '로그인 후 이용이 가능합니다.',
+    });
+  }
+};
