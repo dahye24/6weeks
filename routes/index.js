3fb7ac48ce8314861ef9fb98b1daf5258d69ccef
yunjin5450 <yunjin5450@gmail.com>
2022-10-22 17:57:17 +0900

    쓸모없는 커밋

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,21 @@ const express = require('express');
 const router = express.Router();
 const cookieParser = require('cookie-parser');
 const User = require('./user.route');
-<<<<<<< HEAD
+
 const Post = require('./post.route');
 const Likes = require("./likes.routes");
-const Comment = require('./comment.route');
-=======
->>>>>>> e07761eed8cb8dff143248fdf8284e271066959c
+
+
 
 router.use(cookieParser());
 router.use(express.json());
 
-<<<<<<< HEAD
+
 router.use('/', User);
 router.use('/posts', Post);
 router.use('/likes', Likes);
-router.use('/comments', Comment)
-=======
-router.use(User);
->>>>>>> e07761eed8cb8dff143248fdf8284e271066959c
+
+
 
 module.exports = router;
 
