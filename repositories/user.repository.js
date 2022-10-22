8b321131d08310d246bd5ec21f4bae73f1536a7d
yunjin5450 <yunjin5450@gmail.com>
2022-10-22 17:48:43 +0900

    쓸모없는 커밋

--- repositories/user.repository.js
+++ repositories/user.repository.js
@@ -1,40 +1,10 @@
 const {Users} = require('../models');
 
 class UserRepository {
-<<<<<<< HEAD
-    createUser = async (loginId, nickname, password) => {
-        try {
-            // 유저 정보 생성.
-            const userData = await Users.create({loginId, nickname, password});
-=======
-    createUser = async (loginId, nickname, hash) => {
-        try {
-            // 유저 정보 생성.
-            const userData = await Users.create({loginId, nickname, password: hash});
->>>>>>> e07761eed8cb8dff143248fdf8284e271066959c
-            return userData;
-        } catch (err) {
-            const error = new Error(`잘못된 정보 입니다.`);
-            error.statusCode = 500;
-            throw error;
-        }
-
-    };
-
-    loginUser = async (loginId) => {
-        try {
-            // loginId 와 일치하는 회원 정보 서칭.
-            const userData = await Users.findOne({
-                where: {loginId},
-            });
-            return userData;
-        } catch (err) {
-            const error = new Error(`잘못된 정보 입니다.`);
-            error.statusCode = 500;
-            throw error;
-        }
-
-    };
+  createUser = async (loginId, nickname, password) => {
+    const userData = await Users.create({loginId, nickname, password})
+    return userData;
+  }
 }
 
 module.exports = UserRepository;
