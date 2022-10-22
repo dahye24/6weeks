1f868fdd7cefff23a49c66c9109865acec60b1f8
yunjin5450 <yunjin5450@gmail.com>
2022-10-22 18:10:48 +0900

    레파지토리

--- repositories/user.repository.js
+++ repositories/user.repository.js
@@ -1,10 +1,34 @@
 const {Users} = require('../models');
 
 class UserRepository {
-  createUser = async (loginId, nickname, password) => {
-    const userData = await Users.create({loginId, nickname, password})
-    return userData;
-  }
+
+    createUser = async (loginId, nickname, hash) => {
+        try {
+            // 유저 정보 생성.
+            const userData = await Users.create({loginId, nickname, password: hash});
+            return userData;
+        } catch (err) {
+            const error = new Error(`잘못된 정보 입니다.`);
+            error.statusCode = 500;
+            throw error;
+        }
+
+    };
+
+    loginUser = async (loginId) => {
+        try {
+            // loginId 와 일치하는 회원 정보 서칭.
+            const userData = await Users.findOne({
+                where: {loginId},
+            });
+            return userData;
+        } catch (err) {
+            const error = new Error(`잘못된 정보 입니다.`);
+            error.statusCode = 500;
+            throw error;
+        }
+
+    };
 }
 
 module.exports = UserRepository;
